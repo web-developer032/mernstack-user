cfdbee7b44fa8d0cf4c74e4d3a15d10692da01ef
Mubasher Mukhtar <mubasherali0331@gmail.com>
2023-03-19 01:31:00 +0500

    Fixed user modal

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,6 +8,7 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true },
     username: { type: String, required: true },
     image: { type: String, required: true },
+    password: { type: String, required: true },
 });
 
 const User = mongoose.model("users", userSchema);
